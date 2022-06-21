ec74ce76f90b3c01928719f006cdc4e91fdfaebe
Martin Moreira <martinnmoreira@gmail.com>
2022-06-20 23:07:22 -0300

    cambio en Context

---
+++ src/components/Context/CartContext.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const CartContext = React.createContext()
+const {Provider} = CartContext
+
+const CartProvider = ({children}) => {
+    const [cart, setCart] = React.useState([])
+
+  // addToCart
+    const addToCart = (productDetail, count) => {
+        if(isInCart(productDetail.id)) {
+        const newCart = cart.map(cartItem => {
+            if(cartItem.id === productDetail.id) {
+            cartItem.quantity++
+            }
+            return cartItem
+        })
+        setCart(newCart)
+        }
+        else {
+        setCart([...cart, {...productDetail, quantity: +count}])
+        }
+    }
+    // removeFromCart
+    const removeFromCart = (id) => {
+        //Replicar logica de addToCart pero invertida 
+        //y chequear la funcion a realizar cuando el quantity es 1
+        
+        //SI LA CANTIDAD ES IGUAL A 1 HACE ESTO
+        const newCart = cart.filter((carItem) => carItem.id !== id);
+        setCart(newCart)
+        
+    }
+    // deleteAll
+    const deleteAll = () => {
+        setCart([])
+    }
+    // isInCart
+    const isInCart = (id) => {
+        return cart.find(productDetail => productDetail.id === id)
+    }
+
+  return (
+      <Provider value={{
+          addToCart,
+          removeFromCart,
+          deleteAll,
+          isInCart,
+          cart
+      }}>{children}</Provider>
+    
+  )
+}
+
+
+export {CartContext, CartProvider}
